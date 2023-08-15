c134cdeb5fa25622053b3c75054cba319cf844d2
wmouras <50460997+wmouras@users.noreply.github.com>
2023-08-15 14:06:22 -0300

    manipulação dos eventos

--- src/main/java/com/eventus/api/config/EventusApiConfig.java
+++ src/main/java/com/eventus/api/config/EventusApiConfig.java
@@ -1,17 +1,24 @@
 package com.eventus.api.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 
-@EnableMethodSecurity
+@EnableWebSecurity
 @Configuration
 public class EventusApiConfig {
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public DefaultSecurityFilterChain configurer(HttpSecurity http) throws Exception {
+
+        http
+                .addFilter((servletRequest, servletResponse, filterChain) -> filterChain.doFilter(servletRequest, servletResponse))
+                .csrf(AbstractHttpConfigurer::disable)
+                .anonymous(AbstractHttpConfigurer::disable);
+
+        return http.build();
+
     }
 
 }
